0f61feabd9ab1725c789759f779f08494d0baf3b
Tejas <tejashsankhla@gmail.com>
2023-06-27 00:43:51 +0530

    added city-repository

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { PORT } = require('./config/serverconfig');
 const bodyparser = require("body-parser");
-const SetandStartServer = () => {
+const SetandStartServer =async  () => {
     const app = express();
 
     app.use(bodyparser.json);
     app.use(bodyparser.urlencoded({ extended: true }));
 
-    app.listen(PORT, () => {
+    app.listen(PORT,async () => {
         console.log(`server started at ${PORT}`);
+       
     })
 
 }
