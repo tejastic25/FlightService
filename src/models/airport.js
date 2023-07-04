43aaee76259d5990fa7cfda523b7efecc4d71895
Tejas <tejashsankhla@gmail.com>
2023-07-04 19:04:31 +0530

    added allownull to airport name

--- src/models/airport.js
+++ src/models/airport.js
@@ -14,7 +14,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Airport.init({
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     address: DataTypes.STRING,
     cityid: DataTypes.INTEGER
   }, {
